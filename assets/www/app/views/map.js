cefec3bf1c026b7c5cdd73a9f09d6f8a62be0c16
Wes Williams <wes.williamz@gmail.com>
2012-06-12 14:37:36 -0700

    started to move map code to map.js

--- assets/www/app/views/map.js
+++ assets/www/app/views/map.js
@@ -1,16 +1,16 @@
 marina.googleMap = function(options) {
-  var map = {};
+  var map = {}, googleMap;
 
   map.positionChanged = function(position) {
     var coords = position.coords;
     var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
-    map.googleMap.currentLocation.setPosition(latlng);
+    googleMap.currentLocation.setPosition(latlng);
   };
 
 	map.addLocationMarkerTo = function(opts) {
 		map.currentLocation = new google.maps.Marker({
 			position: opts.latlng,
-			map: map.googleMap,
+			map: googleMap,
 			icon: 'images/sailboat.png',
 			title: "Your location"
 		});
@@ -22,7 +22,7 @@ marina.googleMap = function(options) {
       preserveViewport: true
     };
     var marinaLayer = new google.maps.KmlLayer('https://maps.google.com/maps/ms?ie=UTF8&authuser=0&msa=0&output=kml&msid=217422876588338854635.0004c133df227ae5aa19c', marinaLayerOptions);
-    marinaLayer.setMap(map.googleMap);
+    marinaLayer.setMap(googleMap);
   };
 
   var latlng = new google.maps.LatLng(options.coords.latitude, options.coords.longitude);
@@ -31,7 +31,7 @@ marina.googleMap = function(options) {
     center: latlng,
     mapTypeId: google.maps.MapTypeId.TERRAIN
   };
-  map.googleMap = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+  googleMap = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
   map.addMarinaLayer();
   map.addLocationMarkerTo({latlng: latlng});
 
