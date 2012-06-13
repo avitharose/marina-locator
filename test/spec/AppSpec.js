9fc1bf16ef988e0e41f74ff537f082c5ca849c6f
Wes Williams <wes.williamz@gmail.com>
2012-06-13 15:29:10 -0700

    cleanup spec

--- test/spec/AppSpec.js
+++ test/spec/AppSpec.js
@@ -5,15 +5,27 @@ var appCallback, $ = function(func) {
 var document = function() {
 };
 
-describe("Player", function() {
+describe("Main view creation", function() {
+
+  var view = {};
 
   beforeEach(function() {
     document.addEventListener = jasmine.createSpy();  
+    marina.views.main = jasmine.createSpy();
+    view.show = jasmine.createSpy();
+    marina.views.main.andReturn(view);
   });
 
   it("should listen for deviceReady", function() {
     appCallback();
     expect(document.addEventListener).toHaveBeenCalled();
+  });
+
+  it("should show on deviceReady event", function() {
+    appCallback();
+    document.addEventListener.mostRecentCall.args[1]();
+    expect(marina.views.main).toHaveBeenCalled();
+    expect(view.show).toHaveBeenCalled();
   });
 
 });
