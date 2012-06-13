fdc91af9407e2314764969c0f6bfae3af41eccc3
Wes Williams <wes.williamz@gmail.com>
2012-06-13 11:12:10 -0700

    trying to reduce if and else

--- assets/www/app/views/main.js
+++ assets/www/app/views/main.js
@@ -6,41 +6,48 @@ marina.views.main = function() {
     }
   };
 
-  var success = function(position) {
-    marina.map = marina.googleMap({coords: position.coords});
+  var connected = {
+    geoSuccess: function(position) {
+      marina.map = marina.googleMap({coords: position.coords});
+    }, 
+    
+    geoFail: function(e) {
+      console.log(e);
+      this.geoSuccess(defaultPosition);
+    },
+
+    show: function() {
+      console.log('connected');
+      try {
+        navigator.geolocation.getCurrentPosition(this.geoSuccess, this.getFail);
+      } catch(err) {
+        connected.geoFail(err);
+      }
+    }
   };
 
-  var fail = function(e) {
-    console.log(e);
-    if (marina.util.isConnected()) {
-      success(defaultPosition);
-    } else {
+  var disconnected = {
+    show: function() {
+      console.log('disconnected');
       $('#map_canvas').html('No connection!');
     }
   };
 
-  var connected = function() {
-    console.log('connected');
-    try {
-      navigator.geolocation.getCurrentPosition(success, fail);
-    } catch(err) {
-      console.log(err);
-      success(defaultPosition);
-    }
-  };
-
-  var disconnected = function() {
-    $('#map_canvas').html('No connection!');
-  };
+  var state = function() {
+    document.addEventListener("deviceready", function() {
+      document.addEventListener("online", state().show, false);
+      document.addEventListener("offline", state().show, false);
+    }, false);
+    return function() {
+      if (marina.util.isConnected()) {
+        return connected;
+      }
+      return disconnected;
+    };
+  }();
 
   view.show = function() {
-    if (marina.util.isConnected()) {
-      connected();
-      document.addEventListener("online", connected, false);
-    } else {
-      $('#map_canvas').html('No connection!');
-      document.addEventListener("offline", disconnected, false);
-    }
+    state().show();
   };
 
   return view;
