8282243bb45a4bda215664671ae9505885c39da8
Wes Williams <wes.williamz@gmail.com>
2012-07-05 14:51:36 -0700

    refactoring

--- assets/www/app/models/marina-filter.js
+++ assets/www/app/models/marina-filter.js
@@ -1,6 +1,14 @@
 marina.filter = function() {
   var filter = {};
 
+  filter.nameMatches = function(value, criteria) {
+    return value.name.match(new RegExp(criteria, 'gi'));
+  };
+
+  filter.descriptionMatches = function(value, criteria) {
+    return $(value.description).text().match(new RegExp(criteria, 'gi'));
+  };
+
   filter.byAll = function(values, criteria) {
     var byName = filter.byName(values, criteria);
     var byDesc = filter.byDescription(values, criteria);
@@ -17,7 +25,7 @@ marina.filter = function() {
     var filtered = [], cnt, value;
     for(cnt = 0; cnt < values.length; cnt++) {
       value = values[cnt];
-      if (value.name.match(new RegExp(criteria, 'gi'))) {
+      if (filter.nameMatches(value, criteria)) {
         filtered.push(value);
       }
     }
@@ -28,8 +36,7 @@ marina.filter = function() {
     var filtered = [], cnt, value, description;
     for(cnt = 0; cnt < values.length; cnt++) {
       value = values[cnt];
-      description = $(value.description).text();
-      if (description.match(new RegExp(criteria, 'gi'))) {
+      if (filter.descriptionMatches(value, criteria)) {
         filtered.push(value);
       }
     }
